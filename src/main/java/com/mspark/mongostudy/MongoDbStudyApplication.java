cfe71a2e196799fb990bf9502e51a3b6f5d57d7d
unknown <mspark@wizvera.com>
2020-06-02 10:42:54 +0900

    first commit

---
+++ src/main/java/com/mspark/mongostudy/MongoDbStudyApplication.java
@@ -0,0 +1,13 @@
+package com.mspark.mongostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mspark/mongostudy/MongoDbStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mspark.mongostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
